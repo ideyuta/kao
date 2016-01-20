3fca017c0c40e4a48b97a3966175584af6f9d76e
ideyuta <ide.yuta@gmail.com>
2016-01-21 07:53:37 +0900

    feat(constants): add note action types

--- src/js/renderer/constants/noteActionTypes.js
+++ src/js/renderer/constants/noteActionTypes.js
@@ -4,9 +4,11 @@ import reaction from 'dacho';
  * @type {Object}
  */
 const noteActionTypes = reaction([
+  'FETCH_NOTE',
   'FETCH_NOTE_TITLES',
   'POST_NOTE',
   'POSTED_NOTE',
+  'RECEIVE_NOTE',
   'RECEIVE_NOTE_TITLES'
 ], 'NOTE/');
 
