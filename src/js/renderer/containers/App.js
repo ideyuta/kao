2b646ef2ebe440d0060b1d8310485cae6b05f6f9
ideyuta <ide.yuta@gmail.com>
2016-01-22 07:20:15 +0900

    fix(containers): fix to use the react-router

--- src/js/renderer/containers/App.js
+++ src/js/renderer/containers/App.js
@@ -1,42 +1,12 @@
-import React, {PropTypes} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {
-  fetchNote,
-  fetchNoteTitles,
-  postNote
-} from '../actions/note';
-import NoteListView from '../components/NoteListView';
-
-const propTypes = {
-  fetchNote: PropTypes.func,
-  fetchNoteTitles: PropTypes.func,
-  note: PropTypes.object,
-  postNote: PropTypes.func
-};
+import React from 'react';
+import {Router, Route} from 'react-router';
+import IndexScene from './IndexScene';
+import NoteScene from './NoteScene';
 
 /**
  * App
  */
 export default class App extends React.Component {
-
-  /**
-   * fetch notes
-   */
-  componentDidMount() {
-    this.props.fetchNoteTitles();
-  }
-
-  /**
-   * fetchNotes from post complete
-   *
-   * @param {Object} nextProps - props
-   */
-  componentWillReceiveProps(nextProps) {
-    if (this.props.note.isPosting && !nextProps.note.isPosting) {
-      this.props.fetchNoteTitles();
-    }
-  }
 
   /**
    * render
@@ -45,41 +15,17 @@ export default class App extends React.Component {
    */
   render() {
     return (
-      <div className="Wrapper">
-        <button onClick={this.props.postNote}>Add Post</button>
-        <NoteListView
-          fetchNote={this.props.fetchNote}
-          note={this.props.note}
-        />
-      </div>
+      <Router>
+        <Route
+          component={IndexScene}
+          path="/"
+        >
+          <Route
+            component={NoteScene}
+            path="note/:id"
+          />
+        </Route>
+      </Router>
     );
   }
 }
-
-App.propTypes = propTypes;
-
-/**
- * map state to props
- *
- * @param {Object} state - state
- * @return {Object}
- */
-function mapStateToProps(state) {
-  return {note: state.note};
-}
-
-/**
- * map dispatch to props
- *
- * @param {Function} dispatch - dispatch
- * @return {Object}
- */
-function mapDispachToProps(dispatch) {
-  return {
-    fetchNote: bindActionCreators(fetchNote, dispatch),
-    fetchNoteTitles: bindActionCreators(fetchNoteTitles, dispatch),
-    postNote: bindActionCreators(postNote, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispachToProps)(App);
